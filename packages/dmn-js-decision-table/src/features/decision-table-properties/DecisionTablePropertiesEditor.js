53f2a3227385eac1ecd949e938acf507936bd0df
Philipp Fromme <philippfromme@outlook.com>
2020-10-21 15:17:53 +0200

    WIP

--- packages/dmn-js-decision-table/src/features/decision-table-properties/DecisionTablePropertiesEditor.js
+++ packages/dmn-js-decision-table/src/features/decision-table-properties/DecisionTablePropertiesEditor.js
@@ -4,9 +4,9 @@ const LOW_PRIORITY = 500;
 
 export default class DecisionTableProperties {
   constructor(components) {
-    components.onGetComponent('table.before', LOW_PRIORITY, () => {
-      return Component;
-    });
+    // components.onGetComponent('table.before', LOW_PRIORITY, () => {
+    //   return Component;
+    // });
   }
 }
 
--- packages/dmn-js/test/spec/TabbingSpec.js
+++ packages/dmn-js/test/spec/TabbingSpec.js
@@ -96,7 +96,7 @@ var diagramXML = require('./diagram.dmn');
 
 describe('tabs', function() {
 
-  it('should show tabs', function() {
+  it.only('should show tabs', function() {
 
     var $parent = domify(`
       <div class="test-container">
